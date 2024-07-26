521cbb5584e66fbc75486196f18ed9afcdebe179
ahmed3baas <ahmed.3baas@gmail.com>
2024-07-26 00:30:34 +0000

    add dynamic data and api rout
    add dynamic data and api rout

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template
-
+#https://www.codecademy.com/learn/learn-flask/modules/flask-templates-and-forms/cheatsheet
 app = Flask(__name__)
+JOBS = [{
+    'id': 1,
+    'title': 'Data Scientist',
+    'location': 'giza,egypt',
+    'salary': '15,000 le'
+}, {
+    'id': 2,
+    'title': 'Data Analyst',
+    'location': 'cairo,egypt',
+    'salary': '10,000 le'
+}, {
+    'id': 3,
+    'title': 'Back-End dev',
+    'location': 'alxe,egypt',
+}, {
+    'id': 4,
+    'title': 'Front-End dev',
+    'location': 'mansura,egypt',
+    'salary': '18,000 le'
+}]
 
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html',
+                           jobs=JOBS,
+                           company_name='Ahmed')
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
